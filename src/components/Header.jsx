057d8b8fc6f8ae5501e711789a30f9fb24036b23
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-22 18:40:07 -0800

    finished header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import {NavLink} from 'react-router-dom'
 function Header(){
 
     return (
-        <header class="flex">
-            {/* <img src="" alt="" /> */}
-            <h3>Charles Swayne</h3>
+        <header class="flex w-100">
+            <span>
+                    <h3>Charles Swayne</h3>
+            </span>
+                
 
             <nav>
-                <NavLink to='/'>Portfolio</NavLink>
+                <NavLink className="nav" to='/'>Portfolio</NavLink>
                 <NavLink to='/about'>About Me</NavLink>
                 <NavLink to='/contact'>Contact</NavLink>
                 <NavLink to='/resume'>Resume</NavLink>
